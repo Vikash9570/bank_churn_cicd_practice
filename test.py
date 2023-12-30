7f1a083cbf67b747e385ab8c02fbd26a1b889b94
Vikash <vikashprd30@gmail.com>
2023-12-31 00:21:36 +0530

    add test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+import os
+import sys
+from src.logger import logging
+from src.exception import CustomException
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_ingestion import DataIngestion
+from src.components.model_trainer import ModelTrainer
+from src.pipeline.training_pipeline import calling_function
+
+calling_function()
+
+
+
+
+
+
+
+
